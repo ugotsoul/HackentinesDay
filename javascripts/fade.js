a42eaff1210a505ea3bf8f3f427db38ac2463893
ugotsoul <ugasosheikabdi@gmail.com>
2014-02-11 10:43:41 -0800

    Ecard Project
    Ryan Gosling Valentines day card

---
+++ javascripts/fade.js
@@ -0,0 +1,17 @@
+// code gets installed at the end of the body (after all other HTML)
+(function() {
+
+    var quotes = $(".quotes");
+    var quoteIndex = -1;
+
+    function showNextQuote() {
+        ++quoteIndex;
+        quotes.eq(quoteIndex % quotes.length)
+            .fadeIn(2000)
+            .delay(2000)
+            .fadeOut(2000, showNextQuote);
+    }
+
+    showNextQuote();
+
+})();​
